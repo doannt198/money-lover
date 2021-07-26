a37b2d8ca1f3b1894e84f1eae7b5a5bb2785cc12
DoanNT <nguyentiendoanhn09@gmail.com>
2021-07-26 16:59:32 +0700

    create component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ import React, { Component } from 'react';
+ import './App.css';
+import MainMenu from './components/mainmenu';
+ import Sidebar from './components/sidebar';
+ import HeaderWrap from './components/header-wrap';
+ import Main from './components/main';
+ class App extends Component {
+   render() {
+     return (
+      <div className="container">
+      <div className="container-wrapper">
+        <Sidebar/>
+        <MainMenu></MainMenu>
+        <div className="right">
+          <div className="right-wrapper">
+            <HeaderWrap></HeaderWrap>
+            {/* Main */}
+              <Main></Main>
+          </div>
+        </div>
+      </div>
     </div>
-  );
-}
-
-export default App;
+    
+     );
+   }
+ }
+ 
+ export default App;
